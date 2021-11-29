4aa4608becdbb917b8b92ff31f10ea8f960e09ff
sham <sham.jadhav@shauryatechnosoft.com>
2021-11-29 14:52:24 +0530

    add authguard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ElectionManagementComponent } from './election-management/election-management.component';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthorizationGuard } from './auth/authorization.guard';
+import { NoAuthGuardService } from './auth/no-auth-guard.service';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { PartialComponent } from './partial/partial.component';
 
 const routes: Routes = [
-    // { path: 'create-elections', loadChildren: () => import('./election-management/create-elections/create-elections.module').then(m => m.CreateElectionsModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, 
-    // { path: 'create-constituency', loadChildren: () => import('./create-constitueny/create-constitueny.module').then(m => m.CreateConstituenyModule) }, { path: 'create-regional-leader', loadChildren: () => import('./create-regional-leader/create-regional-leader.module').then(m => m.CreateRegionalLeaderModule) }, { path: 'assign-elections-to-regional-leader', loadChildren: () => import('./assign-elections-to-regional-leader/assign-elections-to-regional-leader.module').then(m => m.AssignElectionsToRegionalLeaderModule) }, { path: 'assign-booth-to-constituency', loadChildren: () => import('./assign-booth-to-constituency/assign-booth-to-constituency.module').then(m => m.AssignBoothToConstituencyModule) }, { path: 'election-dashboard', loadChildren: () => import('./election-dashboard/election-dashboard.module').then(m => m.ElectionDashboardModule) }
-  ];
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule), data: { title: 'Login' }, canActivate: [NoAuthGuardService] },
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthorizationGuard],
+    component: PartialComponent, children: [
+      { path: 'create-elections', loadChildren: () => import('./partial/election-management/create-elections/create-elections.module').then(m => m.CreateElectionsModule) },
+      { path: 'create-constituency', loadChildren: () => import('./partial/election-management/create-constitueny/create-constitueny.module').then(m => m.CreateConstituenyModule) },
+      { path: 'create-regional-leader', loadChildren: () => import('./partial/election-management/create-regional-leader/create-regional-leader.module').then(m => m.CreateRegionalLeaderModule) },
+      { path: 'assign-elections-to-regional-leader', loadChildren: () => import('./partial/election-management/assign-elections-to-regional-leader/assign-elections-to-regional-leader.module').then(m => m.AssignElectionsToRegionalLeaderModule) },
+      { path: 'assign-booth-to-constituency', loadChildren: () => import('./partial/election-management/assign-booth-to-constituency/assign-booth-to-constituency.module').then(m => m.AssignBoothToConstituencyModule) },
+      { path: 'election-dashboard', loadChildren: () => import('./partial/election-management/election-dashboard/election-dashboard.module').then(m => m.ElectionDashboardModule) },
+    ]
+  },
+  { path: '500', component: ServerErrorComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthorizationService } from './auth/authorization.service';
+import { NoAuthGuardService } from './auth/no-auth-guard.service';
+import { PartialComponent } from './partial/partial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartialComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DatePipe, AuthorizationService, NoAuthGuardService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
