ecdacfdb094bc0213b693492a3a0d947b1198e4f
Prasad Shirsat <prasads@STPL.COM>
2021-11-30 10:40:03 +0530

    Latest added access to booth management

--- src/app/partial/booth-management/assign-agents-to-booth/assign-agents-to-booth.module.ts
+++ src/app/partial/booth-management/assign-agents-to-booth/assign-agents-to-booth.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AssignAgentsToBoothRoutingModule } from './assign-agents-to-booth-routing.module';
 import { AssignAgentsToBoothComponent } from './assign-agents-to-booth.component';
-
+import { NgxSelectModule } from 'ngx-select-ex';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,13 @@ import { AssignAgentsToBoothComponent } from './assign-agents-to-booth.component
   ],
   imports: [
     CommonModule,
-    AssignAgentsToBoothRoutingModule
+    AssignAgentsToBoothRoutingModule,
+    NgxSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    MatCheckboxModule,
+    Ng2SearchPipeModule,
   ]
 })
 export class AssignAgentsToBoothModule { }
