213219709878f069ac8b1ea8edd021e75e1acb77
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2021-12-06 19:54:20 +0530

    commit

--- src/app/partial/profile/voters-profile/voters-profile.module.ts
+++ src/app/partial/profile/voters-profile/voters-profile.module.ts
@@ -5,6 +5,7 @@ import { VotersProfileRoutingModule } from './voters-profile-routing.module';
 import { VotersProfileComponent } from './voters-profile.component';
 import { LightboxModule } from '@ngx-gallery/lightbox';
 import { GalleryModule } from '@ngx-gallery/core';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,11 @@ import { GalleryModule } from '@ngx-gallery/core';
     VotersProfileRoutingModule,
     LightboxModule,
     GalleryModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAkNBALkBX7trFQFCrcHO2I85Re2MmzTo8',
+      language: 'en',
+      libraries: ['geometry','places']
+    }),
   ]
 })
 export class VotersProfileModule { }
