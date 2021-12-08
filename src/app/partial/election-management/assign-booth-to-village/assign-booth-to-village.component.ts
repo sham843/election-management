618d920d01d8b6407161331ca1564d127b61e781
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2021-12-08 15:32:12 +0530

    commit

--- src/app/partial/election-management/assign-booth-to-village/assign-booth-to-village.component.ts
+++ src/app/partial/election-management/assign-booth-to-village/assign-booth-to-village.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { CallAPIService } from 'src/app/services/call-api.service';
+import { CommonService } from 'src/app/services/common.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-assign-booth-to-village',
@@ -7,9 +13,141 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignBoothToVillageComponent implements OnInit {
 
-  constructor() { }
+  assemblyArray: any;
+  boothListArray: any;
+  assignBoothsToVillageForm!: FormGroup;
+  submitted = false;
+  allDistrict: any;
+  getTalkaByDistrict: any;
+  resultVillage: any;
+
+  constructor(
+    private spinner: NgxSpinnerService,
+    private callAPIService: CallAPIService,
+    private toastrService: ToastrService,
+    private fb: FormBuilder,
+    private commonService: CommonService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.getDistrict();
+    this.getAssembly();
+    this.defaultForm();
   }
 
+  defaultForm() {
+    this.assignBoothsToVillageForm = this.fb.group({
+      Id: [0],
+      districtId: [''],
+      TalukaId: [''],
+      VillageId: [''],
+      assembly: [''],
+      boothList: [''],
+    })
+  }
+
+  get f() { return this.assignBoothsToVillageForm.controls };
+
+  clearForm(){
+
+  }
+
+  getDistrict() {
+    this.spinner.show();
+    this.callAPIService.setHttp('get', 'Web_GetDistrict_1_0?StateId=' + 1, false, false, false, 'electionServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.allDistrict = res.data1;
+      } else {
+        this.spinner.hide();
+      }
+    }, (error: any) => {
+      if (error.status == 500) {
+        this.router.navigate(['../../500'], { relativeTo: this.route });
+      }
+    })
+  }
+
+  getTaluka() {
+    this.spinner.show();
+    this.callAPIService.setHttp('get', 'Web_GetTaluka_1_0?DistrictId=' + this.assignBoothsToVillageForm.value.districtId, false, false, false, 'electionServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.getTalkaByDistrict = res.data1;
+      } else {
+        this.spinner.hide();
+      }
+    }, (error: any) => {
+      if (error.status == 500) {
+        this.router.navigate(['../../500'], { relativeTo: this.route });
+      }
+    })
+  }
+
+  getVillage() {
+    this.spinner.show();
+    this.callAPIService.setHttp('get', 'Web_GetVillage_1_0?talukaid=' + this.assignBoothsToVillageForm.value.TalukaId, false, false, false, 'electionServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.resultVillage = res.data1;
+      } else {
+        this.spinner.hide();
+      }
+    }, (error: any) => {
+      if (error.status == 500) {
+        this.router.navigate(['../../500'], { relativeTo: this.route });
+      }
+    })
+  }
+
+  getAssembly() {
+    this.spinner.show();
+    this.callAPIService.setHttp('get', 'Web_Election_GetAssembly?UserId=' + this.commonService.loggedInUserId(), false, false, false, 'electionServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.assemblyArray = res.data1;
+      } else {
+        this.spinner.hide();
+        this.assemblyArray = [];
+      }
+    }, (error: any) => {
+      if (error.status == 500) {
+        this.spinner.hide();
+        this.router.navigate(['../500'], { relativeTo: this.route });
+      }
+    })
+  }
+
+  getBoothList() {
+    this.spinner.show();
+    this.callAPIService.setHttp('get', 'Web_Election_GetBoothList?ConstituencyId=' + this.assignBoothsToVillageForm.value.assembly + '&UserId=' + this.commonService.loggedInUserId(), false, false, false, 'electionServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.boothListArray = res.data1;
+      } else {
+        this.spinner.hide();
+        this.boothListArray = [];
+      }
+    }, (error: any) => {
+      if (error.status == 500) {
+        this.spinner.hide();
+        this.router.navigate(['../500'], { relativeTo: this.route });
+      }
+    })
+  }
+  
+  onSubmitData(){
+    console.log(this.assignBoothsToVillageForm.value);
+  }
+
+
 }
+
