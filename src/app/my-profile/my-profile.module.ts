f68386e1b88c9dd1279c49163e83f56cce00bc2d
sham <sham.jadhav@shauryatechnosoft.com>
2021-12-09 12:21:00 +0530

    add my profile page

--- src/app/my-profile/my-profile.module.ts
+++ src/app/my-profile/my-profile.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { MyProfileRoutingModule } from './my-profile-routing.module';
 import { MyProfileComponent } from './my-profile.component';
+import { NgxSelectModule } from 'ngx-select-ex';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 
 @NgModule({
@@ -11,7 +13,10 @@ import { MyProfileComponent } from './my-profile.component';
   ],
   imports: [
     CommonModule,
-    MyProfileRoutingModule
+    MyProfileRoutingModule,
+    NgxSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class MyProfileModule { }
