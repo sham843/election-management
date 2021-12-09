58734434f27ba9b8f87c3ce6dcf5f5ea0447ed92
sham <sham.jadhav@shauryatechnosoft.com>
2021-12-09 10:47:15 +0530

    add my profile page component

---
+++ src/app/partial/settings/restricted-mobile/restricted-mobile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restricted-mobile',
+  templateUrl: './restricted-mobile.component.html',
+  styleUrls: ['./restricted-mobile.component.css']
+})
+export class RestrictedMobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
