7bbf6eccb58ca6ec6de10f43ecc16f033c582c40
sham <sham.jadhav@shauryatechnosoft.com>
2021-12-15 16:55:32 +0530

    voter call entries call api

--- src/app/partial/dialogs/voter-call-entries/voter-call-entries.component.ts
+++ src/app/partial/dialogs/voter-call-entries/voter-call-entries.component.ts
@@ -1,5 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { CallAPIService } from 'src/app/services/call-api.service';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-voter-call-entries',
@@ -7,15 +13,75 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./voter-call-entries.component.css']
 })
 export class VoterCallEntriesComponent implements OnInit {
+  voterId:any
+  
+  getFeedbacksList:any
+  getFeedbacksListTotal:any;
+  feedbacksPaginationNo:any = 1;
+  feedbacksPageSize: number = 10;
+
+
+  enterNewFeedbackForm!:FormGroup;
+
 
   constructor(
     public dialogRef: MatDialogRef<VoterCallEntriesComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private spinner: NgxSpinnerService,
+    private callAPIService: CallAPIService,
+    private router: Router,
+    private route: ActivatedRoute,
+    public dialog: MatDialog,
+    private fb:FormBuilder,
+    ) {}
 
   ngOnInit(): void {
+    this.voterId = this.data.voterId;
+    this.feedbacksList()
   }
 
+  feedbacksList(){
+    this.spinner.show();
+    let obj: any = 'VoterId='+ this.voterId +'&nopage='+this.feedbacksPaginationNo;
+    this.callAPIService.setHttp('get', 'Get_Electioncrm_1_0?' + obj, false, false, false, 'electionServiceForWeb');
+    this.callAPIService.getHttp().subscribe((res: any) => {
+      if (res.data == 0) {
+        this.spinner.hide();
+        this.getFeedbacksList = res.data1;
+        console.log(this.getFeedbacksList);
+        this.getFeedbacksListTotal = res.data2[0].TotalCount;
+      } else {
+        this.getFeedbacksList = [];
+        this.spinner.hide();
+      }
+    }, (error: any) => {
+      this.spinner.hide();
+      if (error.status == 500) {
+        this.router.navigate(['../500'], { relativeTo: this.route });
+      }
+    })
+  }
+
+  onSubmitFeedbackForm(){
+    this.enterNewFeedbackForm = this.fb.group({
+      Id:[],	
+      VoterId:[],	
+      FeedBackDate:	[],
+      FeedBackType:	[],
+      Description:[],	
+      FollowupDate:[],
+      CreatedBy:[],	
+      NotToCall:[]
+    })
+  }
+
+
+  doNotCall(event:any){
+    console.log(event.target.checked)
+  }
   onNoClick(text:any): void {
     this.dialogRef.close(text);
   }
+
+  // default cal
 }
