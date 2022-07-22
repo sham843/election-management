7b7148802fc978f83aa6023178d71b28eab3f808
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2022-07-22 12:32:42 +0530

    add supervisor page changes

--- src/app/partial/booth-management/add-supervisor/add-supervisor.module.ts
+++ src/app/partial/booth-management/add-supervisor/add-supervisor.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AddSupervisorRoutingModule } from './add-supervisor-routing.module';
 import { AddSupervisorComponent } from './add-supervisor.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgxSelectModule } from 'ngx-select-ex';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,11 @@ import { AddSupervisorComponent } from './add-supervisor.component';
   ],
   imports: [
     CommonModule,
-    AddSupervisorRoutingModule
+    AddSupervisorRoutingModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    NgxSelectModule,
+    Ng2SearchPipeModule
   ]
 })
 export class AddSupervisorModule { }
